2e2bc48564151f100e4113f338de6dcb6d5f2cb6
Skyisgood1 <100927571+Skyisgood1@users.noreply.github.com>
2022-06-04 18:01:53 +0900

    2022-06-04

--- JAVA/LAB_1.java
+++ JAVA/LAB_1.java
@@ -1,1 +1,64 @@
 
+public class LAB_1 {
+	//주석
+	public static void main(String[] args) {
+		/*
+		System.out.println("Hello World!");
+		System.out.println("Test");
+		//System.out.println("1");
+		
+		
+		System.out.println("2");
+		System.out.println("3");
+		*/
+		
+		/*
+		int count = 0;
+		count = 10;
+		count = 20;
+		System.out.println(count);
+		double average = 11.1;
+		System.out.println(average);
+		*/
+		
+		/*
+		final int ID = 26205711;
+		// ID = 26205711; (상수는 변경 불가능) 
+		System.out.println(ID);
+		*/
+		
+		/*
+		int x = 59;
+		long bing = 345455433L;
+		float f = 32.4F;
+		double d = 3434343.5;
+		boolean isFun = true;
+		char c = 'a';
+		*/
+		
+		/*	(묵시적 형변환)
+		int x = 50000;
+		long y = x;
+		System.out.println(y);
+		
+		(명시적 강변환)
+		long x2 = 5;	
+		int y2 = (int)x2;
+		System.out.println(y2);
+		*/
+		
+		/*	(묵시적)
+		int intValue = 200;
+		long longvalue = intValue;
+		System.out.println(longvalue);
+		*/
+		
+		/*	(명시적에서 실수로 바꾸고 싶을때)
+		double longValue = 20.0;
+		float intValue = (float)longValue;
+		System.out.println(intValue);
+		*/
+		
+	}
+
+}
