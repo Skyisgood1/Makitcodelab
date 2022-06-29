f3c55c09cffaf82564cb6dc10472195bfcaca582
Skyisgood1 <100927571+Skyisgood1@users.noreply.github.com>
2022-06-29 18:14:02 +0900

    Add files via upload

---
+++ JAVA/LAB001.java
@@ -0,0 +1,69 @@
+import java.util.*;
+public class LAB001 {
+
+	public static void main(String[] args) {
+		/*
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Low? ");
+		int low = sc.nextInt();
+		System.out.print("High? ");
+		int high = sc.nextInt();
+		int sum = 0;
+		for (int i = low; i <= high; i++) {
+			sum += i;
+		}
+		System.out.println("Sum = " + sum);
+		*/
+	}
+
+		/*
+		String str1 = "Frodo Baggins";
+		String str2 = "Gandalf the GRAY";
+		System.out.println(str1.substring(4));
+		System.out.println(str2.substring(3,14));
+		System.out.println(str2.replace("a","oo"));
+		System.out.println(str2.replace("gray","white"));
+		System.out.println("str1".replace("r","range"));
+		
+		*/
+		
+		/*
+		String str2 = "Arcturan Megadonkey";
+		String str3 = "Sirius Cybernetics Corporation";
+		System.out.println(str2.indexOf("donkey"));
+		System.out.println(str2 + str3.charAt(17));
+		System.out.println(str2.toLowerCase().substring(9,13) + str3.substring(18, str3.length() - 7));
+		*/
+		
+		/*
+		String quote = "Four score and seven years ago";
+		System.out.println(quote.toUpperCase().substring(5,10));
+		System.out.print(quote.toLowerCase().substring(0,4));
+		System.out.print(quote.toLowerCase().substring(20,26));
+		*/
+		
+		/*
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Hmmduh? ");
+		double money = sc.nextDouble();
+		
+		System.out.println(money);
+		*/
+		
+		
+		
+		/*
+		int n = sc.nextInt();
+		String s = sc.next();
+		sc.nextLine();
+		String line = sc.nextLine();
+		
+		System.out.println(n);
+		System.out.println(s);
+		System.out.println(line);
+		*/
+	}
+	
+	
+	
+
