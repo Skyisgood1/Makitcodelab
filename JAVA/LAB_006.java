03402b8114c2aefb0937b8e6a38c3fdf791d84e5
Skyisgood1 <100927571+Skyisgood1@users.noreply.github.com>
2022-07-02 18:16:42 +0900

    Add files via upload

---
+++ JAVA/LAB_006.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class LAB_006 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		/*
+		System.out.print("Input the string here: ");
+		
+		String s = sc.next();
+		
+		// Wrong way
+		if (s == "apple") {
+			System.out.println("애플은 맞다.");
+		} else {
+			System.out.println("애플은 아니다.");
+		}
+		
+		
+		// Right way
+		if (s.equals("apple")) {
+			System.out.println("True"); 
+		} else {
+			System.out.println("False");
+		}
+		
+		// Ignoring Upper & Lower case
+		if (s.equalsIgnoreCase("apple")) {
+			System.out.println("True"); 
+		} else {
+			System.out.println("False");
+		}
+		
+		
+		System.out.print("How many numbers do you have? ");
+		int n  = sc.nextInt();
+		double sum = 0;
+		
+		for (int i = 1; i <= n; i ++) {
+			System.out.print("# " + i + " : " );
+			sum += sc.nextDouble();
+		}
+		
+		System.out.println();
+		System.out.println("Sum: " + sum);
+		*/
+		
+		
+		
+		
+		
+	}
+}
+
