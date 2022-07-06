3471cc36043a9d246b0f58c148852a109385d594
Skyisgood1 <100927571+Skyisgood1@users.noreply.github.com>
2022-07-06 18:17:13 +0900

    Add files via upload

---
+++ JAVA/LAB_007.java
@@ -0,0 +1,107 @@
+import java.util.*;
+public class LAB_007 {
+
+	public static void main(String[] args) {
+		
+		/*	18번
+		Scanner sc = new Scanner(System.in);
+		double sum = 0.0;
+		for (int i = 0; i < 4; i++) {
+			sum += sc.nextDouble();
+		}
+		System.out.printf("%.1f\n", sum);
+		
+		
+		 #13번
+		Scanner sc = new Scanner(System.in);
+		for (int i = 0; i < 3; i++) {
+			System.out.print("What color do you want?" );
+			String x =sc.next();
+			
+			if (x.equalsIgnoreCase("B")) {
+				System.out.println("You have chosen Blue. ");
+			} else if (x.equalsIgnoreCase("G")) {
+				System.out.println("You have chosen Green. ");
+			} else if (x.equalsIgnoreCase("R")) {
+				System.out.println("You have chosen Red. ");
+			} else {
+				System.out.println("Unknown color: " + x);
+			}
+	}
+	
+		#14번
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a card: ");
+		String rank = sc.next();
+		String suit = sc.next();
+		
+		String result_1 = "";
+		String result_2 = "";
+			
+		if (rank.equals("2")) {
+			result_1 = "Two";
+		} else if (rank.equals("3")) {
+			result_1 = "Three";
+		} else if (rank.equals("4")) {
+				result_1 = "Four";
+		} else if (rank.equals("5")) {
+			result_1 = "Five";
+		} else if (rank.equals("6")) {
+			result_1 = "Six";
+		} else if (rank.equals("7")) {
+			result_1 = "Seven";
+		} else if (rank.equals("8")) {
+			result_1 = "Eight";
+		} else if (rank.equals("9")) {
+			result_1 = "Nine";
+		} else if (rank.equals("10")) {
+			result_1 = "Ten";
+		} else if (rank.equals("J")) {
+			result_1 = "Jack";
+		} else if (rank.equals("Q")) {
+			result_1 = "Queen";
+		} else if (rank.equals("A")) {
+			result_1 = "Ace";
+		} else if (rank.equals("K")) {
+			result_1 = "King";}
+		
+		if (suit.equals("C")) {
+			result_2 = "Clubs";
+		} else if (suit.equals("D")) {
+			result_2 = "Diamonds";
+		} else if (suit.equals("H")) {
+			result_2 = "Hearts";
+		} else if (suit.equals("S")) {
+			result_2 = "Spades";
+		} 
+		
+
+		#15번	
+		System.out.print(result_1 + " of " + result_2);
+		
+		
+			Scanner sc = new Scanner(System.in);
+			int n = sc.nextInt();
+			System.out.println(countFactors(n));
+	}
+			
+	public static int countFactors(int n) {
+		int cnt = 0;
+		for (int i = 1; i <= n; i++) {
+			if (n % i == 0) {
+				cnt += 1;
+		
+				
+			}
+		}
+		return cnt;
+		*/
+	}
+		
+		
+	
+		
+	
+}
+
+	
